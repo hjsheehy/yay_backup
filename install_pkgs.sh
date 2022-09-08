5d7f8e8abe153951b51b7f73dca1a693ba71ff5b
henryjs <87094102+henryjs@users.noreply.github.com>
2022-09-08 13:50:34 +0100

    deleted:    install_pkgs.sh 	deleted:    list_pkgs.sh

--- install_pkgs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DIR=~/.config/yay
-FILE=$DIR/pkgs
-yay -S $(cat $FILE)
--- list_pkgs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR=~/.config/yay
-FILE=$DIR/pkgs
-mkdir -p $DIR
-yay -Qet | awk '{print $1}' | tr '\n' ' ' > $FILE
